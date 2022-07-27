a918ca1001cabf3d405ff70f1ba084e5e571a4c5
chiranjibismg <chiranjibi_2101cs21@iitp.ac.in>
2022-07-27 14:50:11 +0530

    Implemented and added merge sort and removed exe files

--- sortingalgo.c
+++ sortingalgo.c
@@ -10,6 +10,69 @@ void swap(int *xp, int *yp)
     *xp = *yp;
     *yp = temp;
 }
+
+
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    /* create temp arrays */
+    int L[n1], R[n2];
+ 
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    /* Copy the remaining elements of L[], if there
+    are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    /* Copy the remaining elements of R[], if there
+    are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+    
+        int m = l + (r - l) / 2;
+ 
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+ 
+        merge(arr, l, m, r);
+    }
+}
+ 
 void bubbleSort(int arr[], int n)
 {
     int i, j;
@@ -64,6 +127,7 @@ int main() {
     printf("1.Insertion Sort\n") ;
     printf("2.Selection Sort\n") ;
     printf("3.Bubble Sort\n") ;
+    printf("4.Merge Sort\n") ;
   
     int option ;
     scanf("%d",&option) ;
@@ -103,6 +167,17 @@ int main() {
     if (option ==3)
     {
       bubbleSort(a,n) ;
+      printf("Sorted array is :- ")  ;
+      for ( int i =0 ;i<n ;i++)
+      {
+        printf("%d ",a[i]) ;
+      }
+      printf("\n") ;
+    }
+
+    if (option ==4)
+    {
+      mergeSort(a,0,n-1) ;
       printf("Sorted array is :- ")  ;
       for ( int i =0 ;i<n ;i++)
       {
